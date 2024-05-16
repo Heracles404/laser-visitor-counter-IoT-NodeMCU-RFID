aadde3756f3bebe06505cef9c45bb7c259d125e5
Nazareno Bianca <103508458+Heracles404@users.noreply.github.com>
2024-05-16 11:49:20 +0800

    updated
    update db and pattern log

--- visitor.sql
+++ visitor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2024 at 04:54 AM
+-- Generation Time: May 16, 2024 at 05:48 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -38,7 +38,36 @@ CREATE TABLE `counter` (
 --
 
 INSERT INTO `counter` (`visitor_ID`, `time`, `date`) VALUES
-(0, '2:03 pm', 'may 15 2024');
+(0, '2:03 pm', 'may 15 2024'),
+(6, '05:13:54', '2024-May-16'),
+(7, '05:16:45', '2024-May-16'),
+(8, '05:17:05', '2024-May-16'),
+(9, '05:20:49', '2024-May-16'),
+(10, '05:20:51', '2024-May-16'),
+(11, '05:20:52', '2024-May-16'),
+(12, '05:20:53', '2024-May-16'),
+(13, '05:20:54', '2024-May-16'),
+(14, '05:20:54', '2024-May-16'),
+(15, '05:20:55', '2024-May-16'),
+(16, '05:20:55', '2024-May-16'),
+(17, '05:20:58', '2024-May-16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `state` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`state`) VALUES
+(0);
 
 --
 -- Indexes for dumped tables
@@ -58,7 +87,7 @@ ALTER TABLE `counter`
 -- AUTO_INCREMENT for table `counter`
 --
 ALTER TABLE `counter`
-  MODIFY `visitor_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `visitor_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
