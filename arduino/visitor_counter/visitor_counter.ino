8fa3d4dc10f89a82d805e2a82bfa1f1d9a173e5b
Nazareno Bianca <103508458+Heracles404@users.noreply.github.com>
2024-05-28 17:46:53 +0800

    Update visitor_counter.ino

--- arduino/visitor_counter/visitor_counter.ino
+++ arduino/visitor_counter/visitor_counter.ino
@@ -10,13 +10,13 @@ int visitorDetected;
 #include <ESP8266HTTPClient.h>
 
 // Bianca WiFi
-// const char* ssid = "IoT";
-// const char* password = "AccessPoint.2024";
-// const char* host = "http://192.168.68.100";
+const char* ssid = "IoT";
+const char* password = "AccessPoint.2024";
+const char* host = "http://192.168.119.196";
 
-const char* ssid = "ACM2";
-const char* password = "0495452821@2024";
-const char* host = "http://192.168.1.42";
+// const char* ssid = "ACM2";
+// const char* password = "0495452821@2024";
+// const char* host = "http://192.168.1.42";
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
